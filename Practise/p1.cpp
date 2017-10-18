e2eb08c139e11e700ab561683856174c6ce0753d
Daniel Pieczewski <dpiec005@uottawa.ca>
2017-10-18 15:37:41 -0400

    Started practising c++

---
+++ Practise/p1.cpp
@@ -0,0 +1,21 @@
+//
+// Created by daniel on 18/10/17.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    const double PI = 3.1415;
+    cout << "PI is equal to " << PI << endl;
+
+    const double e = 2.7182;
+    cout << "e is equal to " << e << endl;
+
+    cout << "All together, e is equal to " << e << " PI is equal to " << PI <<endl;
+
+    return 0;
+
+}
+

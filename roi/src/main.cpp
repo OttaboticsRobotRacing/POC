cd349c959b226a9d7fd2adb8241e1abacbc192a6
jimmy <livingoscilloscope@gmail.com>
2017-11-23 23:51:14 -0500

    added roi test; wip

---
+++ roi/src/main.cpp
@@ -0,0 +1,17 @@
+#include "opencv2/opencv.hpp"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+using namespace cv;
+
+
+int main()
+{
+    Mat img = imread("test.jpg", 1);
+
+
+
+    return 0;
+}

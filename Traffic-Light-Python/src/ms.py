5b03d88975a0dce9d117c67e079a0f74314f377a
Lucas Anderson <lucasanderson1999@gmail.com>
2018-03-28 19:26:21 -0400

    Successful first test with multiscale template matching script

--- Traffic-Light-Python/src/ms.py
+++ Traffic-Light-Python/src/ms.py
@@ -3,29 +3,29 @@ import cv2
 import imutils
 from matplotlib import pyplot as plt
 
-template = cv2.imread('tl_temp.jpg')
+template = cv2.imread('tl_temp2.jpg')
 #template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 #template = cv2.Canny(template, 50, 200)
 (tH, tW) = template.shape[:2]
 
 #template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
 
-image = cv2.imread("tl_red.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.imread("tl_green.jpg")
+#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 found = None
 
 for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-    resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
-    r = gray.shape[1] / float(resized.shape[1])
+    resized = imutils.resize(image, width = int(image.shape[1] * scale))
+    r = image.shape[1] / float(resized.shape[1])
     if resized.shape[0] < tH or resized.shape[1] < tW:
-		break
-    result = cv2.matchTemplate(resized, template, cv2.TM_SQDIFF)
+		  break
+    result = cv2.matchTemplate(resized, template, cv2.TM_CCOEFF)
     (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
 
-    clone = np.dstack([resized, resized, resized])
-    cv2.rectangle(clone, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-    cv2.imshow("Visualize", clone)
-    cv2.waitKey(0)
+    #clone = np.dstack([resized, resized, resized])
+    cv2.rectangle(resized, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+    #cv2.imshow("Visualize", resized)
+    #cv2.waitKey(0)
 
     if found is None or maxVal > found[0]:
 	    found = (maxVal, maxLoc, r)
@@ -35,6 +35,12 @@ for scale in np.linspace(0.2, 1.0, 20)[::-1]:
 (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
 cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 
-plt.imshow(template)
-plt.imshow(image)
-plt.show()
+cv2.imshow("template", template)
+cv2.imshow("image", image)
+cv2.waitKey(0)
+#plt.imshow(template)
+#plt.imshow(image)
+#plt.show()
+
+#cap.release()
+#cv2.destroyAllWindows()

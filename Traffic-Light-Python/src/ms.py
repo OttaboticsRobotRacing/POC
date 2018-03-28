fb502355c65982c52a4acd506a73a497c7385ef4
Lucas Anderson <lucasanderson1999@gmail.com>
2018-03-28 19:39:24 -0400

    Added camera stream

--- Traffic-Light-Python/src/ms.py
+++ Traffic-Light-Python/src/ms.py
@@ -3,18 +3,23 @@ import cv2
 import imutils
 from matplotlib import pyplot as plt
 
-template = cv2.imread('tl_temp2.jpg')
-#template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-#template = cv2.Canny(template, 50, 200)
-(tH, tW) = template.shape[:2]
+cap = cv2.VideoCapture(0)
 
-#template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
+while (True):
+  ret, image = cap.read()
 
-image = cv2.imread("tl_green.jpg")
-#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-found = None
+  template = cv2.imread('tl_temp2.jpg')
+  #template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+  #template = cv2.Canny(template, 50, 200)
+  (tH, tW) = template.shape[:2]
 
-for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+  #template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
+
+  #image = cv2.imread("tl_green.jpg")
+  #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  found = None
+
+  for scale in np.linspace(0.2, 1.0, 20)[::-1]:
     resized = imutils.resize(image, width = int(image.shape[1] * scale))
     r = image.shape[1] / float(resized.shape[1])
     if resized.shape[0] < tH or resized.shape[1] < tW:
@@ -30,17 +35,19 @@ for scale in np.linspace(0.2, 1.0, 20)[::-1]:
     if found is None or maxVal > found[0]:
 	    found = (maxVal, maxLoc, r)
 
-(_, maxLoc, r) = found
-(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
-cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+  (_, maxLoc, r) = found
+  (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+  (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+  cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 
-cv2.imshow("template", template)
-cv2.imshow("image", image)
-cv2.waitKey(0)
+  cv2.imshow("template", template)
+  cv2.imshow("image", image)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
 #plt.imshow(template)
 #plt.imshow(image)
 #plt.show()
 
-#cap.release()
-#cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
